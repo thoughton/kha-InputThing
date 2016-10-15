ac261b76abe95ffca821478bcbebf8d81092dc9f
thoughton <dev@thoughton.net>
2016-10-15 17:33:32 +0100

    Added asset loading step on initialise (for the fonts). Added onRender callback. Added Zui instantiation. Added array for the input axes. Added test drawing of fonts using unicode chars at various sizes. Added some test Zui slider and text elements (all currently commented out).

--- Sources/Main.hx
+++ Sources/Main.hx
@@ -3,11 +3,17 @@
 package;
 
 import kha.System;
+import kha.Scheduler;
+import kha.Assets;
 
 class Main {
 	public static function main() {
 		System.init({title:"Input Thing", width:1024, height:768}, function () {
-			new Project();
+			Assets.loadEverything(function () {
+				var project = new Project();
+				Scheduler.addTimeTask(project.onUpdate, 0.0, (1.0 / 60.0));
+				System.notifyOnRender(project.onRender);
+			});
 		});
 	}
 }
--- Sources/Project.hx
+++ Sources/Project.hx
@@ -2,16 +2,84 @@
 
 package;
 
+import kha.Assets;
 import kha.input.Gamepad;
+//import kha.Font;
+import kha.Framebuffer;
+import zui.Zui;
+//import zui.Id;
 
 class Project {
 	var m_kDeadZone (default,never) : Float = 0.08;
+	var m_axes : Array<Float>;
+	var m_zui : Zui;
 
 	public function new() {
 		trace("-[ Input Thing ]-");
 
-		var pad = Gamepad.get();
-		pad.notify(onGamepadAxis, onGamepadButton);
+		m_axes = new Array();
+
+		//m_zui = new Zui(Assets.fonts.OpenSans, 17, 16, 0, 1.0);
+		//m_zui = new Zui(Assets.fonts.Roboto_Regular, 17, 16, 0, 1.0);
+		m_zui = new Zui(Assets.fonts.NDS12, 25, 13, 0, 1.0, 1.0);
+
+		Gamepad.get().notify(onGamepadAxis, onGamepadButton);
+	}
+
+	public function onUpdate():Void {
+
+	}
+
+	public function onRender(framebuffer:Framebuffer):Void {
+		var g = framebuffer.g2;
+		g.begin();
+		{
+			// Draw non-UI things...
+
+			g.font = Assets.fonts.NDS12;
+			g.fontGlyphs.push(0x2660);  // Spades
+			g.fontGlyphs.push(0x2668);  // Hot springs
+
+			var y = 50;
+			var s = 10;
+			for (i in 0...24)
+			{
+				g.fontSize = (s + (i * 1));
+				//g.drawString("♠"+" Click axis to calibrate. (Font size = "+g.fontSize+".)", 50.0, (y + (i * (20 + (i * 0.3)))));
+				//g.drawString("♨"+" Click axis to calibrate. (Font size = "+g.fontSize+".)", 50.0, (y + (i * (20 + (i * 0.3)))));
+				g.drawString("♨" + " ♠ (Font size = " + g.fontSize + ")", 50.0, (y + (i * (20 + (i * 0.3)))));
+			}
+		}
+		g.end();
+
+		/*
+		m_zui.begin(g);
+		{
+			var x = 0;
+			var y = 0;
+			var w = framebuffer.width;
+			var h = framebuffer.height;
+
+			if (m_zui.window(Id.window(), x, y, w, h, Zui.LAYOUT_VERTICAL)) {
+				if (m_zui.button("Hello")) {
+					trace("World");
+				}
+
+				var aCount = 0;
+				for (a in m_axes)
+				{
+					aCount++;
+					m_zui.text(("" + a));
+					m_zui.slider(Id.slider(), ("Axis " + aCount), -1.0, 1.0, true, 1000, a, true);
+				}
+
+				m_zui.slider(Id.slider(), "Temp.", -1.0, 1.0, true, 1000, 0, true);
+
+				m_zui.text("♠ Click axis to calibrate.");
+			}
+		}
+		m_zui.end();
+		*/
 	}
 	
 	public function onGamepadAxis(axis:Int, value:Float):Void {
@@ -19,6 +87,8 @@ class Project {
 		{
 			trace("Axis " + axis + ": " + value);
 		}
+
+		m_axes[axis] = value;
 
 		/*
 		///LEFT ANALOG STICK
