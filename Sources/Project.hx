dd8276a65d4ba814886af23d680206c5119211a4
thoughton <dev@thoughton.net>
2016-10-15 20:06:42 +0100

    Added code and comment to demonstrate the issue with Zui instantiation seemingly breaking unicode glyph rendering at the font size is gets passed into its constructor.

--- Sources/Project.hx
+++ Sources/Project.hx
@@ -4,10 +4,8 @@ package;
 
 import kha.Assets;
 import kha.input.Gamepad;
-//import kha.Font;
 import kha.Framebuffer;
 import zui.Zui;
-//import zui.Id;
 
 class Project {
 	var m_kDeadZone (default,never) : Float = 0.08;
@@ -18,10 +16,12 @@ class Project {
 		trace("-[ Input Thing ]-");
 
 		m_axes = new Array();
-
-		//m_zui = new Zui(Assets.fonts.OpenSans, 17, 16, 0, 1.0);
-		//m_zui = new Zui(Assets.fonts.Roboto_Regular, 17, 16, 0, 1.0);
-		m_zui = new Zui(Assets.fonts.NDS12, 25, 13, 0, 1.0, 1.0);
+		
+		// Issue:
+		// 	The font sizes passed to Zui here cause the unicode glyphs to fail to render when g.drawString is called for text of either of these sizes.
+		// 	Changing these sizes changes the size of unicode glyphs that fail to draw.
+		// 	Commenting out or removing this Zui instatiation will re-enable unicode glyph rendering at all font sizes. 
+		m_zui = new Zui(Assets.fonts.NDS12, 24, 12, 0, 1.0, 1.0);
 
 		Gamepad.get().notify(onGamepadAxis, onGamepadButton);
 	}
@@ -34,169 +34,28 @@ class Project {
 		var g = framebuffer.g2;
 		g.begin();
 		{
-			// Draw non-UI things...
-
 			g.font = Assets.fonts.NDS12;
 			g.fontGlyphs.push(0x2660);  // Spades
 			g.fontGlyphs.push(0x2668);  // Hot springs
 
-			var y = 50;
-			var s = 10;
+			var sizeBase = 10;
+			var sizeStep = 1;
+			var x = 50;
+			var yBase = 50;
 			for (i in 0...24)
 			{
-				g.fontSize = (s + (i * 1));
-				//g.drawString("♠"+" Click axis to calibrate. (Font size = "+g.fontSize+".)", 50.0, (y + (i * (20 + (i * 0.3)))));
-				//g.drawString("♨"+" Click axis to calibrate. (Font size = "+g.fontSize+".)", 50.0, (y + (i * (20 + (i * 0.3)))));
-				g.drawString("♨" + " ♠ (Font size = " + g.fontSize + ")", 50.0, (y + (i * (20 + (i * 0.3)))));
+				g.fontSize = (sizeBase + (i * sizeStep));
+				g.drawString(("♨" + " ♠ (Font size = " + g.fontSize + ")"), x, (yBase + (i * (20 + (i * 0.3)))));
 			}
 		}
 		g.end();
-
-		/*
-		m_zui.begin(g);
-		{
-			var x = 0;
-			var y = 0;
-			var w = framebuffer.width;
-			var h = framebuffer.height;
-
-			if (m_zui.window(Id.window(), x, y, w, h, Zui.LAYOUT_VERTICAL)) {
-				if (m_zui.button("Hello")) {
-					trace("World");
-				}
-
-				var aCount = 0;
-				for (a in m_axes)
-				{
-					aCount++;
-					m_zui.text(("" + a));
-					m_zui.slider(Id.slider(), ("Axis " + aCount), -1.0, 1.0, true, 1000, a, true);
-				}
-
-				m_zui.slider(Id.slider(), "Temp.", -1.0, 1.0, true, 1000, 0, true);
-
-				m_zui.text("♠ Click axis to calibrate.");
-			}
-		}
-		m_zui.end();
-		*/
 	}
 	
 	public function onGamepadAxis(axis:Int, value:Float):Void {
-		if ((value < -m_kDeadZone) || (value > m_kDeadZone))
-		{
-			trace("Axis " + axis + ": " + value);
-		}
-
 		m_axes[axis] = value;
-
-		/*
-		///LEFT ANALOG STICK
-		if (axis == 0){
-			if (value < -0.5){
-				trace('LEFT');
-			} else if (value > 0.5){
-				trace('RIGHT');
-			}
-		}
-		if (axis == 1){
-			if (value > 0.5){
-				trace('UP');
-			} else if (value < -0.5){
-				trace('DOWN');
-			}
-		}
-		
-		///RIGHT ANALOG STICK
-		if (axis == 3){
-			if (value > 0.5){
-				trace('LEFT');
-			} else if (value < -0.5){
-				trace('RIGHT');
-			}
-		}
-		if (axis == 4){
-			if (value < -0.5){
-				trace('UP');
-			} else if (value > 0.5){
-				trace('DOWN');
-			}
-		}
-		
-		if (axis == 2){
-			if (value < -0.5){
-				trace('LEFT TRIGGER');
-			}
-		}
-		
-		if (axis == 5){
-			if (value < -0.5){
-				trace('RIGHT TRIGGER');
-			}
-		}
-		*/
 	}
 	
 	public function onGamepadButton(button:Int, value:Float):Void {
-		if ((value < -m_kDeadZone) || (value > m_kDeadZone))
-		{
-			trace("Button " + button + ": " + value);
-		}
-		
-		/*
-		///ABXY BUTTONS
-		if (button == 0){
-			trace('A');
-		} else if (button == 1){
-			trace('B');
-		} else if (button == 2){
-			trace('X');
-		} else if (button == 3){
-			trace('Y');
-		}
-		
-		///BUMPER BUTTONS
-		if (button == 4){
-			trace('LEFT BUMPER');
-		} else if (button == 5){
-			trace('RIGHT BUMPER');
-		}
-		
-		///ANALOG PRESS
-		if (button == 6){
-			trace('LEFT ANALOG PRESS');
-		} else if (button == 7){
-				trace('RIGHT ANALOG PRESS');
-		}
-		
-		///START/BACK BUTTONS
-		if (button == 8){
-			trace('START BUTTON');
-		} else if (button == 9){
-			trace('BACK BUTTON');
-		} else if (button == 10){
-			trace('HOME BUTTON');
-		}
-		
-		///DPAD BUTTONS
-		if (button == 11){
-			trace('DPAD UP');
-		} else if (button == 12){
-			trace('DPAD DOWN');
-		} else if (button == 13){
-			trace('DPAD LEFT');
-		} else if (button == 14){
-			trace('DPAD RIGHT');
-		}
-		
-		///HOME BUTTON
-		if (button == 15){
-			trace('euhenuihe');
-		} else if (button == 16){
-			trace('sueitehuinteu');
-		} else if (button == 17){
-			trace('euineunitue');
-		}
-		*/
+
 	}
 }
